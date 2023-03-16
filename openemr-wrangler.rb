4a768265ac37a7df4f94b6dc66a68665dd09d955
jachnlin <110345504+jachnlin@users.noreply.github.com>
2023-03-16 12:12:53 +0000

    More indentation fixes

--- openemr-wrangler.rb
+++ openemr-wrangler.rb
@@ -10,3 +10,4 @@ require 'json'
 
 # this takes a hash of options, almost all of which map directly
 # to the familiar database.yml in rails
+# See http://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/MysqlAdapter.html
