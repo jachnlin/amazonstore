09cc5c4ea0d5fa3e065cbbe443bf8b3791a39e9f
jachnlin <110345504+jachnlin@users.noreply.github.com>
2023-03-17 06:28:03 +0000

    added new images

--- openemr-wrangler.rb
+++ openemr-wrangler.rb
@@ -11,3 +11,4 @@ require 'json'
 # this takes a hash of options, almost all of which map directly
 # to the familiar database.yml in rails
 # See http://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/MysqlAdapter.html
+client = Mysql2::Client.new(:host => "localhost", :port => "3306", :username => "root", :password => "rootpw", :database => "openemr")
